a658d26e80af5657dd9b1a16af3dcc2d6d0fdfc0
Divyang <divyang0024@gmail.com>
2026-03-09 18:39:02 +0530

    small tweak

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,6 +1,10 @@
 import { io } from "socket.io-client";
-const socket = io("http://localhost:5000", {
+
+const BACKEND_URL = "https://mendly-ai.vercel.app" || "http://localhost:5000";
+
+const socket = io(BACKEND_URL, {
   withCredentials: true,
   auth: { token: localStorage.getItem("token") },
 });
+
 export default socket;
